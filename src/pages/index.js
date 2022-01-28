6e724770ab6481bdf101e6e1574314d4c6cd5d2f
samy7777 <mandalsandeep670@gmail.com>
2022-01-28 19:47:41 +0530

    who we are section

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,11 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import WhoWeAre from './about/WhoWeAre';
 
 export default function Home() {
   return (
   <>
-  <h1>Header</h1>
+  {/* <h1>Header</h1> */}
+  <WhoWeAre/>
   </>
   )
 }
