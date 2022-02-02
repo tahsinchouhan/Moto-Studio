3265107d79afe2e8f4835de2e6c29f05e8d47b3b
monika <monikaevdt@gmail.com>
2022-02-02 13:02:12 +0530

    subHeader mouseover ui

--- src/pages/subHeader/HealthPersonalCare.js
+++ src/pages/subHeader/HealthPersonalCare.js
@@ -1,12 +1,66 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col, Navbar, Nav, NavDropdown } from "react-bootstrap";
+
+import Image from "next/image";
+import six from "../../../public/images/six.png";
+import Seekpng from "../../../public/images/SeekPng.png";
 
 function HealthPersonalCare() {
-    return (
-        <div>
-           <h1>HealthPersonalCare
-HealthPersonalCare</h1> 
+  return (
+    <div>
+      <div className="container-fluid">
+        <div className="container">
+          <div className="all-shop-list p-3 m-3">
+            <Row className="offset-sm-1">
+              <Row>
+                <Col md={3} lg={3} className="text-center">
+                  <div className="six-list">
+                    <Image src={Seekpng} alt="Seekpng" />
+                  </div>
+                </Col>
+
+                <Col md={3} lg={3} className="text-center">
+                  <div className="">
+                    <div className="six-list">
+                      <h6 className="Grocery-foods">Herbal Soaps</h6>
+                    </div>
+                    <div>
+                      <span className="Wildforest">Aloe Vera Soap</span>
+                      <br />
+                      <span className="Wildforest">Goat Milk Soap</span>
+                      <br />
+                      <span className="Wildforest">Lemon Soap</span>
+                      <br />
+                    </div>
+                  </div>
+                </Col>
+                <Col md={3} lg={3} className="text-center">
+                  <div>
+                    <Image src={six} alt="six" />
+                  </div>
+                </Col>
+                <Col md={3} lg={3} className="text-center">
+                  <div className="six-list">
+                    <div>
+                      <h6 className="Grocery-foods">Other Products</h6>
+                    </div>
+                    <div>
+                      <span className="Wildforest">Clear Skin Face Pack</span>
+                      <br />
+                      <span className="Wildforest">Handmade Green Tea</span>
+                      <br />
+                      <span className="Wildforest">Ayurvedic Powders</span>
+                      <br />
+                    </div>
+                  </div>
+                </Col>
+              </Row>
+            </Row>
+          </div>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
 
-export default HealthPersonalCare
+export default HealthPersonalCare;
