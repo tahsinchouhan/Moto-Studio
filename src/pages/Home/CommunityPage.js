1202f7ec95ac89a4ae94cdabb689d31c43a951f1
Subhasmita sahoo <Subhasahoo9889@gmail.com>
2022-02-03 12:49:12 +0530

    add CommunityPage

--- src/pages/Home/CommunityPage.js
+++ src/pages/Home/CommunityPage.js
@@ -1,15 +1,26 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import Image from "next/image";
-import img1 from "../../../public/Picture/picture.png";
+import image1 from "../../Assets/images/home/image1.png"; 
+import image2 from "../../Assets/images/home/image2.png"; 
+import image3 from "../../Assets/images/home/image3.png"; 
+import image4 from "../../Assets/images/home/image4.png"; 
+import image5 from "../../Assets/images/home/image5.png"; 
+import image6 from "../../Assets/images/home/image6.png"; 
+
+
+
+
 function CommunityPage() {
   return (
     <>
       <Container>
-        <div className="main-community">
-          <h1 className="Text text-center">The Community</h1>
-          <hr className="Line " />
+        <div className="main-community text-md-center text-start">
+          <h1 className="Text">The Community</h1>
+          <hr className="Line my-4 " />
           <div className="para-div">
+
+         
             <p className="Content ">
               Far far away, behind the word mountains, far from the countries
               Vokalia and Consonantia, there live the blind texts. Separated
@@ -19,100 +30,84 @@ function CommunityPage() {
             </p>
           </div>
         </div>
-        <div>
-          <Row>
-            <Col md={4} sm={12} className="image1 Gallery">
-              <Image
-                src="/Image/image1.png"
-                alt="Picture of the author"
-                width={572}
-                height={381}
-              />
-              <div className="overlay">
-                <h1 className="d-flex flex-column align-items-center fw-bold"></h1>
-                <span></span>
-              </div>
-            </Col>
-            <Col md={4} sm={12} className="image2 Gallery">
-              <Image
-                src="/Image/image2.png "
-                alt="Picture of the author"
-                width={572}
-                height={381}
-              />
-              <div className="overlay">
-                <h1 className="d-flex flex-column align-items-center fw-bold"></h1>
-                <span></span>
-              </div>
-            </Col>
 
-            <Col md={4} sm={12} className="image3 Gallery">
-              <Image
-                src="/Image/image3.png "
-                alt="Picture of the author"
-                width={572}
-                height={381}
-              />
-              <div className="overlay">
-                <h1 className="d-flex flex-column align-items-center fw-bold"></h1>
-                <span></span>
-              </div>
-            </Col>
-          </Row>
-        </div>
+        <Row>
+          <Col lg={4} md={6} className="image1 Gallery">
+            <Image
+              src={image1}
+              alt="Picture of the author"
+              width={572}
+              height={381}
+            />
 
-        <div className="image-gap">
-          <Row>
-            <Col md={4} sm={12} className="image4 Gallery">
-              <Image
-                src="/Image/image4.png"
-                alt="Picture of the author"
-                width={572}
-                height={381}
-              />
-              <div className="overlay">
-                <h1 className="d-flex flex-column align-items-center fw-bold"></h1>
-                <span></span>
-              </div>
-            </Col>
-            <Col md={4} sm={12} className="image5 Gallery">
-              <Image
-                src="/Image/image5.png "
-                alt="Picture of the author"
-                width={572}
-                height={381}
-              />
-              <div className="overlay">
-                <h1 className="d-flex flex-column align-items-center fw-bold"></h1>
-                <span></span>
-              </div>
-            </Col>
+            <div className="overlay">
+              <p className="Text-name">Pure <br/> Wildforest <br/> Honey</p>
+            </div>
+          </Col>
+          <Col lg={4} md={6} className="image1 Gallery">
+            <Image
+              src={image2}
+              
+              alt="Picture of the author"
+              width={572}
+              height={381}
+            />
+            <div className="overlay">
+              <p className="Text-name">Premium  <br/> Cashews</p>
+            </div>
+          </Col>
 
-            <Col md={4} sm={12} className="image6 Gallery">
-              <Image
-                src="/Image/image6.png "
-                alt="Picture of the author"
-                width={572}
-                height={381}
-                className="img-fluid"
-              />
+          <Col lg={4} md={6} className="image1 Gallery">
+            <Image
+              src={image3}
+              alt="Picture of the author"
+              width={572}
+              height={381}
+            />
+            <div className="overlay">
+              <p className="Text-name">Chyawan <br/> Prash </p>
+            </div>
+          </Col>
 
-              <div className="overlay">
-                <h1 className="d-flex flex-column align-items-center fw-bold"></h1>
-                <span>Amla Murabba</span>
-              </div>
-            </Col>
-          </Row>
-        </div>
+          <Col lg={4} md={6} className="image1 Gallery">
+            <Image
+              src={image4}
+              alt="Picture of the author"
+              width={572}
+              height={381}
+            />
+            <div className="overlay">
+              <p className="Text-name">Mahua  <br/> Laddu</p>
+            </div>
+          </Col>
+          <Col lg={4} md={6} className="image1 Gallery">
+            <Image
+              src={image5}
+              alt="Picture of the author"
+              width={572}
+              height={381}
+            />
+            <div className="  overlay">
+              <p className="Text-name">Mahua Cookies</p>
+            </div>
+          </Col>
+
+          <Col lg={4} md={6} className="image1 Gallery">
+            <Image
+              src={image6}
+              alt="Picture of the author"
+              width={572}
+              height={381}
+              // className="img-fluid "
+            />
+
+            <div className="overlay align-items-center">
+              <p className="Text-name ">Amla  <br/> Murabba </p>
+              
+            </div>
+          </Col>
+        </Row>
       </Container>
-     
-
-      <div className="Frame1">
-        <Image src={img1} alt="Picture of the author" className="img-fluid" />
-        <a className="Follow">FOLLOW US</a>
-      </div>
-
-      
     </>
   );
 }
