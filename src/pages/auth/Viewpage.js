c9c773887d1e1e092153fa6d74ed59c62178d95a
Ajay Dewangan <ajaydewangan215@gmail.com>
2022-02-15 19:12:34 +0530

    Ajay - Updated

--- src/pages/auth/Viewpage.js
+++ src/pages/auth/Viewpage.js
@@ -24,27 +24,27 @@ function Viewpage({ productData }) {
         </div>
       </div>
       <hr />
-      {productData?.doc?.product_?.map((product) => {
+      {productData?.doc?.products_?.map((product) => {
         return <div className="row" key={product?._id}>
             <div className="col col-sm-12 col-md-6 col-lg-6 col-xl-6">
               <div className="d-flex p-1">
                 <div>
-                  <Image src={flower} alt="" width={136} height={130} />
+                  <Image src={product?.images?.length > 0 ? product?.images[0]?.img || flower : flower} alt="" width={136} height={130} />
                 </div>
                 <div className="p-3">
-                  <span>190592</span>
-                  <p className="dreaming-midnight-x1"> dreaming midnight x1,</p>
+                  <span>{product?.products?.title || ''}</span>
+                  <p className="dreaming-midnight-x1">{product?.products?.SKU_Number}</p>
                 </div>
               </div>
             </div>
             <div className="col col-sm-12 col-md-3 col-lg-3 col-xl-3">
               <div>
-                <span>3</span>
+                <span>{product?.quantity || ''}</span>
               </div>
             </div>
             <div className="col col-sm-12 col-md-3 col-lg-3 col-xl-3">
               <div>
-                <span>₹10,500.00</span>
+                <span>₹ {product?.price || ''}</span>
               </div>
             </div>
           </div>
