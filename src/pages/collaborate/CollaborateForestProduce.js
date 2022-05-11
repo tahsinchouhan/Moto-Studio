46b459c916a250986eefa1550ee396452eee1310
Vinitpal <vs361017@gmail.com>
2022-05-11 12:19:21 +0530

    build fix

--- src/pages/collaborate/CollaborateForestProduce.js
+++ src/pages/collaborate/CollaborateForestProduce.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import { Row, Col, Container } from "react-bootstrap";
-import CollaborateForestImg from "../../assets/images/collaborate/collaborateForest.svg";
+import CollaborateForestImg from "../../assets/images/collaborate/CollaborateForest.svg";
 
 function CollaborateForestProduce() {
   return (
