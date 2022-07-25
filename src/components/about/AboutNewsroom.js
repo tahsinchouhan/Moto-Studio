755dd0ad242431740c9b2e6708e17c1db27a6f34
Sagar Soni <dusksoni@gmail.com>
2022-07-25 15:57:46 +0530

    read more about page

--- src/components/about/AboutNewsroom.js
+++ src/components/about/AboutNewsroom.js
@@ -1,11 +1,11 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 import { Row, Col, Container } from "react-bootstrap";
 import Image from "next/image";
-import Button from "../button/ButtonLight"
+import Button from "../button/ButtonLight";
 import Microenterprises from "../../assets/images/about/microenterprises.png";
-import { apipath } from '../../pages/api/apiPath';
-import Link from 'next/link';
-import IntoNewsroom from '../../pages/news';
+import { apipath } from "../../pages/api/apiPath";
+import Link from "next/link";
+import IntoNewsroom from "../../pages/news";
 
 function AboutNewsroom() {
   const [newsData, setNewsData] = useState([]);
@@ -15,20 +15,20 @@ function AboutNewsroom() {
       try {
         const res = await fetch(`${apipath}/api/v1/newsroom/list`);
         const objData = await res.json();
-        setNewsData(objData?.data)
+        setNewsData(objData?.data);
       } catch (error) {
         console.log(error);
       }
-    }
+    };
     fetchData();
-  }, [])
-console.log('news data is: ',newsData);
+  }, []);
+  console.log("news data is: ", newsData);
 
   return (
     <>
       <div className="about-newsroom-container">
         <Row className="d-flex about-newsroom-gutter justify-content-center">
-          <Col xs={12} className="mx-auto px-0" style={{marginTop:"-26px"}}>
+          <Col xs={12} className="mx-auto px-0" style={{ marginTop: "-26px" }}>
             <p className="about-newsroom-heading">Newsroom</p>
             <hr className="about-newsroom-hr mt-3" />
           </Col>
@@ -37,40 +37,48 @@ console.log('news data is: ',newsData);
 
       <Container>
         <Row className=" row-cols-1 row-cols-md-3 g-4 mt-4 mb-5 justify-content-center">
-          {
-            newsData.length && newsData?.slice(0,3)?.map(news => {
-              return <div className="col-lg-4 col-md-6" key={news._id}>
-                <div className="card h-100 border-2">
-                  <Image
-                    src={news.images[0]?.img || Microenterprises}
-                    className="w-100 card-img-top"
-                    alt="Microinterprises"
-                    width={300}
-                    height={300}
-                    unoptimized={true}
-                    loading="eager"
-                    objectFit='cover'
-                  />
-                  <div className="card-body">
-                    <h5 className="card-title">{news.title}</h5>
-                    <small className="about-newsroom-date">{new Date(news.date).toDateString()}</small>
-                    <p className="card-text about-newsroom-card-para mt-3">{news.content}</p>
-                    {/* <Button className="mb-2 mt-3 about-newsroom-button" text="READ MORE" /> */}
-                    {/* <Link href={news.url_customize}>
+          {newsData.length &&
+            newsData?.slice(0, 3)?.map((news) => {
+              return (
+                <div className="col-lg-4 col-md-6" key={news._id}>
+                  <div className="card h-100 border-2">
+                    <Image
+                      src={news.images[0]?.img || Microenterprises}
+                      className="w-100 card-img-top"
+                      alt="Microinterprises"
+                      width={300}
+                      height={300}
+                      unoptimized={true}
+                      loading="eager"
+                      objectFit="cover"
+                    />
+                    <div className="card-body">
+                      <h5 className="card-title">{news.title}</h5>
+                      <small className="about-newsroom-date">
+                        {new Date(news.date).toDateString()}
+                      </small>
+                      <p className="card-text about-newsroom-card-para mt-3">
+                        {news.content}
+                      </p>
+                      {/* <Button className="mb-2 mt-3 about-newsroom-button" text="READ MORE" /> */}
+                      {/* <Link href={news.url_customize}>
                     <a >
                       <Button className="mb-2 mt-3 about-newsroom-button" text="READ MORE" />
                     </a>
                     </Link> */}
-                    <Link href={`about/${news._id}`}>
-                    <a >
-                      <Button className="mb-2 mt-3 about-newsroom-button" text="READ MORE" />
-                    </a>
-                    </Link>
+                      <Link href={`news/${news._id}`}>
+                        <a>
+                          <Button
+                            className="mb-2 mt-3 about-newsroom-button"
+                            text="READ MORE"
+                          />
+                        </a>
+                      </Link>
+                    </div>
                   </div>
                 </div>
-              </div>
-            })
-          }
+              );
+            })}
         </Row>
       </Container>
     </>
