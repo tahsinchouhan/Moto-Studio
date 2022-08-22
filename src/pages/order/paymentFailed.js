322c8e7289434bdcf0c7d2f0bcf1625637f40533
Ajay Dewangan <ajaydewangan215@gmail.com>
2022-08-22 11:55:29 +0530

    failed page

---
+++ src/pages/order/paymentFailed.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import Image from "next/image";
+import failed from '../../../public/images/failed.png';
+import Router from 'next/router';
+
+function paymentFailed() {
+    return (
+        <div className="card m-auto m-5 p-5">
+            <div className="card-body text-center p-5 mb-5">
+              <Image
+                src={failed}
+                alt="tick"
+                width={100}
+                height={100}
+              />
+                <h2 className={'text-success fw-bold mt-4'}>Payment Failed</h2>
+            </div>
+            <style jsx>{`
+             .card-body {
+               font-family: 'Lora';
+             }
+            `}           
+            </style>
+        </div>
+    )
+}
+
+export default paymentFailed
