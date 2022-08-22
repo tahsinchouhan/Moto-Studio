05b1bf7c5e5025167f83d900def99e772dd6a8df
Ajay Dewangan <ajaydewangan215@gmail.com>
2022-08-22 11:48:32 +0530

    updated

--- src/pages/shopping/Shopping.js
+++ src/pages/shopping/Shopping.js
@@ -517,12 +517,12 @@ function Shopping() {
       <input
         type="hidden"
         name="surl"
-        value="https://www.chhattisgarhherbals.org/order/OrderConfirmed"
+        value="https://chhattisgarhherbals.org/order/OrderConfirmed"
       />
       <input
         type="hidden"
         name="furl"
-        value="https://www.chhattisgarhherbals.org/order/paymentFailed"
+        value="https://chhattisgarhherbals.org/order/paymentFailed"
       />
       <input type="hidden" name="phone" />
       <input type="hidden" name="hash" />
