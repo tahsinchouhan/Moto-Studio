36f388497d8a834d8b9e0d1e7756b5f44a5d5d9a
Tahsin <tahsinchouhan2000@gmail.com>
2022-10-07 16:43:27 +0530

    testing on local host

--- src/pages/shopping/Shopping.js
+++ src/pages/shopping/Shopping.js
@@ -178,7 +178,10 @@ function Shopping({ weightData }) {
   const getToken = async () => {
     try {
       // const res = await fetch(apipath + `/api/v1/order/shiprocket/token`);
-      const res = await fetch(apipath + `/api/v1/order/shiprocket/token`);
+      // const res = await fetch(apipath + `/api/v1/order/shiprocket/token`);
+      const res = await fetch(
+        `http://localhost:3019/api/v1/order/shiprocket/token`
+      );
       const data = await res.json();
       setToken(data.data.token);
       localStorage.setItem("ship-token", data.data.token);
@@ -425,60 +428,64 @@ function Shopping({ weightData }) {
     const data = createOrder.data.data;
     const token = await getToken();
     try {
-      const res = await fetch(`${apipath}/api/v1/order/shiprocket/order`, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${Token}`,
-        },
-        body: JSON.stringify({
-          order_id: data.order_id,
-          order_date: data.createdAt,
-          pickup_location: "Sundar Nagar",
-          channel_id: "2939322",
-          comment: "",
-          billing_customer_name: data.billingAddress?.first_name,
-          billing_last_name: data.billingAddress?.last_name,
-          billing_address: data.billingAddress?.address,
-          billing_address_2: data.billingAddress?.address,
-          billing_city: data.billingAddress?.city,
-          billing_pincode: data.billingAddress?.pincode,
-          billing_state: data.billingAddress?.state,
-          billing_country: data.billingAddress?.country,
-          billing_email: data.billingAddress?.email,
-          billing_phone: data.billingAddress?.mobile,
-          shipping_is_billing: true,
-          shipping_customer_name: data.shippingAddress?.gift_firstname,
-          shipping_last_name: data.shippingAddress?.gift_lastname,
-          shipping_address: data.shippingAddress?.gift_address,
-          shipping_address_2: data.shippingAddress?.gift_address,
-          shipping_city: data.shippingAddress?.gift_city,
-          shipping_pincode: data.shippingAddress?.gift_pincode,
-          shipping_country: data.shippingAddress?.gift_country,
-          shipping_state: data.shippingAddress?.gift_state,
-          shipping_email: data.shippingAddress?.gift_email,
-          shipping_phone: data.shippingAddress?.gift_mobile,
-          order_items: data.products_.map((item) => ({
-            name: item.product_id,
-            sku: item._id,
-            units: item.quantity,
-            selling_price: item.price,
-            discount: item.discount_value,
-            tax: item.taxable_amount,
-            hsn: 1,
-          })),
-          payment_method: data.payment_status,
-          shipping_charges: data.total_shippingAmount,
-          giftwrap_charges: 0,
-          transaction_charges: 0,
-          total_discount: 0,
-          sub_total: data.total_amount,
-          length: 10,
-          breadth: 15,
-          height: 20,
-          weight: 2.5,
-        }),
-      });
+      // const res = await fetch(`${apipath}/api/v1/order/shiprocket/order`, {
+      const res = await fetch(
+        `http://localhost:3019/api/v1/order/shiprocket/order`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${Token}`,
+          },
+          body: JSON.stringify({
+            order_id: data.order_id,
+            order_date: data.createdAt,
+            pickup_location: "Sundar Nagar",
+            channel_id: "2939322",
+            comment: "",
+            billing_customer_name: data.billingAddress?.first_name,
+            billing_last_name: data.billingAddress?.last_name,
+            billing_address: data.billingAddress?.address,
+            billing_address_2: data.billingAddress?.address,
+            billing_city: data.billingAddress?.city,
+            billing_pincode: data.billingAddress?.pincode,
+            billing_state: data.billingAddress?.state,
+            billing_country: data.billingAddress?.country,
+            billing_email: data.billingAddress?.email,
+            billing_phone: data.billingAddress?.mobile,
+            shipping_is_billing: true,
+            shipping_customer_name: data.shippingAddress?.gift_firstname,
+            shipping_last_name: data.shippingAddress?.gift_lastname,
+            shipping_address: data.shippingAddress?.gift_address,
+            shipping_address_2: data.shippingAddress?.gift_address,
+            shipping_city: data.shippingAddress?.gift_city,
+            shipping_pincode: data.shippingAddress?.gift_pincode,
+            shipping_country: data.shippingAddress?.gift_country,
+            shipping_state: data.shippingAddress?.gift_state,
+            shipping_email: data.shippingAddress?.gift_email,
+            shipping_phone: data.shippingAddress?.gift_mobile,
+            order_items: data.products_.map((item) => ({
+              name: item.product_id,
+              sku: item._id,
+              units: item.quantity,
+              selling_price: item.price,
+              discount: item.discount_value,
+              tax: item.taxable_amount,
+              hsn: 1,
+            })),
+            payment_method: data.payment_status,
+            shipping_charges: data.total_shippingAmount,
+            giftwrap_charges: 0,
+            transaction_charges: 0,
+            total_discount: 0,
+            sub_total: data.total_amount,
+            length: 10,
+            breadth: 15,
+            height: 20,
+            weight: 2.5,
+          }),
+        }
+      );
       const result = await res.json();
       console.log("shiprocket result is:", result);
     } catch (error) {
